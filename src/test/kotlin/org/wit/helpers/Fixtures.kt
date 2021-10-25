7937108fd7758b253c0327c7af54831c7af80ec2
mabdulha <mozeebabdulha@gmail.com>
2021-10-25 18:23:09 +0100

    Implementing first test which saves hard coded users from the fixtures to the db

--- src/test/kotlin/org/wit/helpers/Fixtures.kt
+++ src/test/kotlin/org/wit/helpers/Fixtures.kt
@@ -7,8 +7,8 @@ val validFName = "Michael"
 val validEmail = "testuser1@test.com"
 
 val users = arrayListOf(
-    UserDTO(fname = "Alice", lname = "Cullen", email = "alice@cullen.com", password = "secret", weight = 2.0, height = 1.55f, gender = 'M', age = 25, id = 0),
-    UserDTO(fname = "Bob", lname = "Cat", email = "bob@cat.ie", password = "secret", weight = 2.0, height = 1.555f, gender = 'F', age = 18, id = 1),
-    UserDTO(fname = "Mary", lname = "Contrary", email = "mary@contrary.com", password = "secret", weight = 2.0, height = 1.555f, gender = 'F', age = 55, id = 2),
-    UserDTO(fname = "Carol", lname = "Singer", email = "carol@singer.com", password = "secret", weight = 2.0, height = 1.555f, gender = 'M', age = 32, id = 3)
+    UserDTO(fname = "Alice", lname = "Cullen", email = "alice@cullen.com", password = "secret", weight = 2.0, height = 170f, gender = 'M', age = 25, id = 1),
+    UserDTO(fname = "Bob", lname = "Cat", email = "bob@cat.ie", password = "secret", weight = 2.0, height = 1.555f, gender = 'F', age = 18, id = 2),
+    UserDTO(fname = "Mary", lname = "Contrary", email = "mary@contrary.com", password = "secret", weight = 2.0, height = 1.555f, gender = 'F', age = 55, id = 3),
+    UserDTO(fname = "Carol", lname = "Singer", email = "carol@singer.com", password = "secret", weight = 2.0, height = 1.555f, gender = 'M', age = 32, id = 4)
 )
--- src/test/kotlin/org/wit/repository/UserDAOTest.kt
+++ src/test/kotlin/org/wit/repository/UserDAOTest.kt
@@ -1,8 +1,13 @@
 package org.wit.repository
 
-import org.jetbrains.exposed.sql.Database
-import org.junit.jupiter.api.BeforeAll
 import org.wit.helpers.users
+import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.Assertions.assertEquals
+import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Test
+import org.wit.db.Users
 
 //retrieving some test data from Fixtures
 val user1 = users[0]
@@ -18,6 +23,25 @@ class UserDAOTest {
         @JvmStatic
         internal fun setupInMemoryDatabaseConnection() {
             Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
+        }
+    }
+
+    @Test
+    fun `multiple users added to table can be retrieved successfully`() {
+        transaction {
+
+            //Arrange - create and populate table with three users
+            SchemaUtils.create(Users)
+            val userDAO = UserDAO()
+            userDAO.save(user1)
+            userDAO.save(user2)
+            userDAO.save(user3)
+
+            //Act & Assert
+            assertEquals(3, userDAO.getAll().size)
+            assertEquals(user1, userDAO.findById(user1.id))
+            assertEquals(user2, userDAO.findById(user2.id))
+            assertEquals(user3, userDAO.findById(user3.id))
         }
     }
 
