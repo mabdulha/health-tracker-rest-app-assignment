d20f75115eef0d629b3dcffce98e31215bb6efc5
mabdulha <mozeebabdulha@gmail.com>
2021-11-01 20:56:45 +0000

    First integration test implemented to get all users

--- src/test/kotlin/org/wit/controllers/HealthTrackerAPITest.kt
+++ src/test/kotlin/org/wit/controllers/HealthTrackerAPITest.kt
@@ -1,5 +1,8 @@
 package org.wit.controllers
 
+import kong.unirest.Unirest
+import org.junit.Assert.assertEquals
+import org.junit.jupiter.api.Test
 import org.junit.jupiter.api.TestInstance
 import org.wit.config.DbConfig
 import org.wit.helpers.ServerContainer
@@ -10,5 +13,11 @@ class HealthTrackerAPITest {
     private val db = DbConfig().getDbConnection()
     private val app = ServerContainer.instance
     private val origin = "http://localhost:" + app.port()
+
+    @Test
+    fun `get all users from the database returns 200 or 404 response`() {
+        val response = Unirest.get("$origin/api/users/").asString()
+        assertEquals(200, response.status)
+    }
 
 }
