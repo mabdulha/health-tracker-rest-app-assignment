53de25bdf4f8a54087edb33387233da69d61a2a0
mabdulha <mozeebabdulha@gmail.com>
2021-11-06 19:13:45 +0000

    Implemented method to save exercise to the database

--- src/main/kotlin/org/wit/repository/ExerciseDAO.kt
+++ src/main/kotlin/org/wit/repository/ExerciseDAO.kt
@@ -1,5 +1,6 @@
 package org.wit.repository
 
+import org.jetbrains.exposed.sql.insert
 import org.jetbrains.exposed.sql.select
 import org.jetbrains.exposed.sql.selectAll
 import org.jetbrains.exposed.sql.transactions.transaction
@@ -35,6 +36,20 @@ class ExerciseDAO {
             Exercises
                 .select {Exercises.userId eq userId}
                 .map { mapToExerciseDTO(it) }
+        }
+    }
+
+    //Save an exercise to the database
+    fun save(exerciseDTO: ExerciseDTO){
+        transaction {
+            Exercises.insert {
+                it[name] = exerciseDTO.name
+                it[description] = exerciseDTO.description
+                it[calories] = exerciseDTO.calories
+                it[duration] = exerciseDTO.duration
+                it[muscle] = exerciseDTO.muscle
+                it[userId] = exerciseDTO.userId
+            }
         }
     }
 
