ec34843f3f89ee3ce10758568bcb9fe03ad6d418
mabdulha <mozeebabdulha@gmail.com>
2021-11-17 15:48:29 +0000

    Added first test to add ingredients and read them

--- src/test/kotlin/org/wit/repository/IngredientDAOTest.kt
+++ src/test/kotlin/org/wit/repository/IngredientDAOTest.kt
@@ -1,7 +1,13 @@
 package org.wit.repository
 
 import org.jetbrains.exposed.sql.Database
+import org.jetbrains.exposed.sql.SchemaUtils
+import org.jetbrains.exposed.sql.transactions.transaction
+import org.junit.jupiter.api.Assertions.assertEquals
 import org.junit.jupiter.api.BeforeAll
+import org.junit.jupiter.api.Nested
+import org.junit.jupiter.api.Test
+import org.wit.db.Ingredients
 import org.wit.helpers.ingredients
 
 //retrieving some test data from Fixtures
@@ -19,5 +25,38 @@ class IngredientDAOTest {
             Database.connect("jdbc:h2:mem:test", driver = "org.h2.Driver", user = "root", password = "")
         }
     }
+
+    @Nested
+    inner class CreateIngredients {
+
+        @Test
+        fun `multiple ingredients added to table can be retrieved successfully`() {
+            transaction {
+
+                //Arrange - create and populate table with three ingredients
+                SchemaUtils.create(Ingredients)
+                val ingredientDao = IngredientDAO()
+                ingredientDao.save(ingredient1)
+                ingredientDao.save(ingredient2)
+                ingredientDao.save(ingredient3)
+
+                //Act & Assert
+                assertEquals(3, ingredientDao.getAll().size)
+                assertEquals(ingredient1, ingredientDao.findByIngredientId(ingredient1.id))
+                assertEquals(ingredient2, ingredientDao.findByIngredientId(ingredient2.id))
+                assertEquals(ingredient3, ingredientDao.findByIngredientId(ingredient3.id))
+            }
+        }
+
+    }
+
+    @Nested
+    inner class ReadIngredients {}
+
+    @Nested
+    inner class UpdateIngredients {}
+
+    @Nested
+    inner class DeleteIngredients {}
 
 }
